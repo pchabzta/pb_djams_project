dc258a79b50a05e2c3c26e8a586885b8b9c78d67
Preecha <pbootwicha@gmail.com>
2018-09-12 11:24:56 +0700

    pb_djams_project

--- pb_djams_project/settings.py
+++ pb_djams_project/settings.py
@@ -134,6 +134,7 @@ USE_TZ = False  # TO USE LOCALTIME NOT TIME AWARE
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
